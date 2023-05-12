42c8c5220ccdb2f255de255981ffb4ea6ae043fe
Anderson Gabriel Ferreira <anderson.gabriel.ferreira@gmail.com>
2023-05-12 19:56:22 -0300

    Initial Commit

---
+++ app/src/main/java/br/com/gabrielorander/multi_module_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.gabrielorander.multi_module_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
