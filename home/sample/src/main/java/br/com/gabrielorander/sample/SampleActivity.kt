b870d4de7c64672c04b9b5c8119d8193064ec8f2
Anderson Gabriel Ferreira <anderson.gabriel.ferreira@gmail.com>
2023-05-27 22:58:07 -0300

    Controle de navegação.
    Adiciona controle de navegação tanto para ActionBar quanto para o back nativo do android.

--- home/sample/src/main/java/br/com/gabrielorander/sample/SampleActivity.kt
+++ home/sample/src/main/java/br/com/gabrielorander/sample/SampleActivity.kt
@@ -2,15 +2,37 @@ package br.com.gabrielorander.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import br.com.gabrielorander.sample.databinding.ActivitySampleBinding
 
 class SampleActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySampleBinding
+    private val navController by lazy {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        setSupportActionBar(binding.toolbar)
+        navHostFragment.navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySampleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if(!navController.popBackStack()){
+            super.onBackPressed()
+        }
     }
 }
